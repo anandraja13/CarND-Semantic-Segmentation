0bea3fa7e24d6cd1bc06f5ca822c9e548847db47
Anand <anandraja13@gmail.com>
2019-04-07 08:55:12 -0400

    Improved detection using kernel initialization

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
+    # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
@@ -54,36 +55,43 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
+    # TODO: Implement function
     
     # 1x1 convolution to match depth with num_classes
     conv7_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
-                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                kernel_initializer= tf.truncated_normal_initializer(stddev=0.01))
     
     # upsample
     output = tf.layers.conv2d_transpose(conv7_1x1, num_classes, 4, (2,2), padding='same',
-                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                       kernel_initializer= tf.truncated_normal_initializer(stddev=0.01))
     
     # 1x1 convolution to match depth with num_classes
     conv4_1x1 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same',
-                                 kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                 kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                 kernel_initializer= tf.truncated_normal_initializer(stddev=0.01))
     
     # skip connection
     output = tf.add(output, conv4_1x1)
     
     # upsample
     output = tf.layers.conv2d_transpose(output, num_classes, 4, (2,2), padding='same',
-                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                         kernel_initializer= tf.truncated_normal_initializer(stddev=0.01))
     
     # 1-by-1 convolution to match depth with num_classes
     conv3_1x1 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same',
-                                 kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                 kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                 kernel_initializer= tf.truncated_normal_initializer(stddev=0.01))
     
     # skip connection
     output = tf.add(output, conv3_1x1)
     
     # upsample
     output = tf.layers.conv2d_transpose(output, num_classes, 16, (8,8), padding='same',
-                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                       kernel_initializer= tf.truncated_normal_initializer(stddev=0.01))
     
     return output
 tests.test_layers(layers)
@@ -98,7 +106,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    
+    # TODO: Implement function
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     labels = tf.reshape(correct_label, (-1, num_classes))
     
@@ -141,7 +149,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
                                feed_dict={input_image: image, 
                                           correct_label: label, 
                                           keep_prob: 0.5, 
-                                          learning_rate: 0.0009})
+                                          learning_rate: 0.0001})
             print("Loss {:.5f}".format(loss))
         
     
@@ -163,8 +171,8 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
-    epochs     = 50
-    batch_size = 10
+    epochs     = 25
+    batch_size = 4
     
     with tf.Session() as sess:
         # Path to vgg model
@@ -175,6 +183,7 @@ def run():
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
+        # TODO: Build NN using load_vgg, layers, and optimize function
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
         learning_rate = tf.placeholder(tf.float32, name='learning_rate')
         
